660bda5af93639a62da52c96a8323d6acfc3a613
Sarah M Brown <brownsarahm@uri.edu>
2024-03-25 15:26:41 -0400

    make puja an an approved approver

--- cspt/config.py
+++ cspt/config.py
@@ -3,5 +3,5 @@ from datetime import datetime
 REPO = 'http://compsys-progtools.github.io/spring2024/'
 BASE_URL = 'https://raw.githubusercontent.com/compsys-progtools/spring2024/main/_'
 
-GH_APPROVERS = ['brownsarahm','trevmoy','yussif-issah','marcinpawlukiewicz']
+GH_APPROVERS = ['brownsarahm','trevmoy','yussif-issah','marcinpawlukiewicz','puja-g9']
 EARLY_BIRD_DEADLINE= datetime.fromisoformat('2024-02-19')
