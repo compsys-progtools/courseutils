369993603e512a425291c5b9e34a8b21e37f4b4c
Sarah M Brown <brownsarahm@uri.edu>
2024-09-05 11:14:09 -0400

    f24

--- cspt/config.py
+++ cspt/config.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 # UPDATE: update this each semester
-REPO = 'http://compsys-progtools.github.io/spring2024/'
-BASE_URL = 'https://raw.githubusercontent.com/compsys-progtools/spring2024/main/_'
+REPO = 'http://compsys-progtools.github.io/fall024/'
+BASE_URL = 'https://raw.githubusercontent.com/compsys-progtools/fall2024/main/_'
 
-GH_APPROVERS = ['brownsarahm','trevmoy','yussif-issah','marcinpawlukiewicz','puja-g9']
-EARLY_BIRD_DEADLINE= datetime.fromisoformat('2024-02-19')
+GH_APPROVERS = ['brownsarahm','marcinpawlukiewicz','VioletVex']
+EARLY_BIRD_DEADLINE= datetime.fromisoformat('2024-09-20')
