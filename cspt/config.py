1f28abd28e1941d74ea9f416d7bfb68c4989a716
Sarah M Brown <brownsarahm@uri.edu>
2025-09-18 22:07:15 -0400

    add thomas, closes #11

--- cspt/config.py
+++ cspt/config.py
@@ -4,8 +4,9 @@ from datetime import timedelta
 # UPDATE: update this each semester
 REPO = 'http://compsys-progtools.github.io/fall2025/'
 BASE_URL = 'https://raw.githubusercontent.com/compsys-progtools/fall2025/main/_'
+GLOSSARY_URL = 'https://compsys-progtools.github.io/fall2025/glossary/#term-'
 
-GH_APPROVERS = ['AymanBx','brownsarahm',]
+GH_APPROVERS = ['AymanBx','brownsarahm','thomaspeck11']
 EARLY_BIRD_DEADLINE= datetime.fromisoformat('2025-09-25')
 
 def expand_range(first_day,last_day=None,days_of_week=[]):
@@ -21,7 +22,7 @@ class CourseDates():
     # scheduling choice
     meeting_days =[1,3] # datetime has 0=Monday
     meeting_hour = 12 # cutoff to ttreat prev at this hour
-    lab_day = 1
+    lab_day = 0 # 0=Monday
     lab_hour = 14
     # -------semester settings from academic calender 
     #  https://web.uri.edu/academic-calendars/
@@ -30,7 +31,8 @@ class CourseDates():
 
     #  add any skipped days or ranges (without makeup)
     # single days must be  tuple, (have a ,)
-    no_class_ranges = [(dt(2025,11,11),),
+    no_class_ranges = [(dt(2025,10,13),),
+                       (dt(2025,11,11),),
                        (dt(2025,11,26),dt(2025,11,30)),]
     
     
@@ -40,13 +42,21 @@ class CourseDates():
     penalty_free_end = first_day + timedelta(days=21)
     early_bird_deadline = first_day + timedelta(days=21)
     def __init__(self):
+        # set class days
         skipped_days = ([day for date_range in self.no_class_ranges for day in expand_range(*date_range)] +
                         list(self.date_substitutes.values() )  )
         
         possible_list = expand_range(self.first_day,self.last_day,self.meeting_days)
         # if not skipped, check if replaced otherwise use that date
-        self.class_meetings = [self.date_substitutes.get(m,m) for m in possible_list if not(m in skipped_days)]
+        self.class_meetings = [self.date_substitutes.get(m,m) for m in possible_list 
+                               if not(m in skipped_days)]
         self.class_meeting_strings = [m.isoformat() for m in self.class_meetings]
+
+        # set lab days 
+        possible_labs = expand_range(self.first_day,self.last_day,[self.lab_day])
+        self.lab_meetings = [self.date_substitutes.get(m,m) for m in possible_labs 
+                             if not(m in skipped_days)]
+        self.lab_meeting_strings = [m.isoformat() for m in self.lab_meetings]
     
 
     def prev_class(self,today):
